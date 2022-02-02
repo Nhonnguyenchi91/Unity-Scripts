2e386db970ce9f408f9e8023df39521eba7f33ab
Nhonnguyenchi91 <98295243+Nhonnguyenchi91@users.noreply.github.com>
2022-02-02 19:34:29 +0700

    Update Graphic_Raycaster_Example.cs

--- Scripts/Graphic_Raycaster_Example.cs
+++ Scripts/Graphic_Raycaster_Example.cs
@@ -5,36 +5,27 @@ using System.Collections.Generic;
 
 public class Graphic_Raycaster_Example : MonoBehaviour //Để sử dụng thì script này phải được add vào Canvas
 {
-    GraphicRaycaster m_Raycaster;
-    PointerEventData m_PointerEventData;
-    EventSystem m_EventSystem;
+    GraphicRaycaster m_Raycaster; // Khởi tạo biến m_Raycaster là một GraphicRaycaster của Canvas
+    PointerEventData m_PointerEventData; // Khởi tạo biến m_PointerEventData là event từ con trỏ pointer (mouse/touch)
+    EventSystem m_EventSystem; // Khởi tạo biến m_EventSystem thuộc EventSystem
 
     void Start()
     {
-        //Fetch the Raycaster from the GameObject (the Canvas)
-        m_Raycaster = GetComponent<GraphicRaycaster>();
-        //Fetch the Event System from the Scene
-        m_EventSystem = GetComponent<EventSystem>();
+        m_Raycaster = GetComponent<GraphicRaycaster>(); // Gán Component GraphicRaycaster của Canvas vào biến m_Raycaster
+        m_EventSystem = GetComponent<EventSystem>(); // Gán Component EventSystem vào biến m_EventSystem
     }
 
     void Update()
     {
-        //Check if the left Mouse button is clicked
+        // Kiểm tra nếu Con trỏ pointer đang ở trên một GameObject
         if (EventSystem.current.IsPointerOverGameObject())
         {
-            //Set up the new Pointer Event
-
-            m_PointerEventData = new PointerEventData(m_EventSystem);
-            //Set the Pointer Event Position to that of the mouse position
-            m_PointerEventData.position = Input.mousePosition;
-
-            //Create a list of Raycast Results
-            List<RaycastResult> results = new List<RaycastResult>();
-            Debug.Log("B"+results.Count);
+            m_PointerEventData = new PointerEventData(m_EventSystem); // Khởi tạo một Pointer Event mới
+            m_PointerEventData.position = Input.mousePosition; // Đặt vị trí của Pointer Event là vị trí của con trỏ
+            List<RaycastResult> results = new List<RaycastResult>(); //Tạo một list RatcastResult để lưu các kết quả Raycast
             //Raycast using the Graphics Raycaster and mouse click position
-            m_Raycaster.Raycast(m_PointerEventData, results);
-            Debug.Log("A" + results.Count);
-            //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+            m_Raycaster.Raycast(m_PointerEventData, results); // Tiến hành Raycast tại vị trí của con trỏ và lưu kết quả trả về vào list RaycastResult
+            //Với các kết quả trong list RaycastResult ta sẽ in ra tên các GameObject mà phép Raycast đã phát hiện ra.
             foreach (RaycastResult result in results)
             {
                 Debug.Log("Hit " + result.gameObject.name);
